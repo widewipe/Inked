28f9ddf437ff2a2e5879ebac6267baf2c72149ca
Amartya Chowdhury <work.amar05@yahoo.com>
2024-07-30 22:16:06 +0530

    fix dark mode

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -22,7 +22,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className="h-full flex dark:bg-[#111]">
+    <div className="h-full flex dark:bg-[#1f1f1f]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
